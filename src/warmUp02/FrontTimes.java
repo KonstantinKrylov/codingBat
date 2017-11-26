543483368face7a46bb2b215e38d64ce9ab86bf2
KonstantinKrylov <kr-konstantin@yandex.ru>
2017-11-26 23:12:11 +0300

    warmUp02

---
+++ src/warmUp02/FrontTimes.java
@@ -0,0 +1,33 @@
+package warmUp02;
+
+public class FrontTimes {
+    /**
+     Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars,
+     or whatever is there if the string is less than length 3. Return n copies of the front;
+
+     frontTimes("Chocolate", 2) → "ChoCho"
+     frontTimes("Chocolate", 3) → "ChoChoCho"
+     frontTimes("Abc", 3) → "AbcAbcAbc"
+     */
+
+    public static void main(String[] args) {
+        System.out.println(frontTimes("Chocolate", 2));
+    }
+
+    public static String frontTimes(String str, int n) {
+
+        String resultS = "";
+
+        if(str.length() < 3){
+            str = str;
+        } else {
+            str = str.substring(0,3);
+        }
+
+        while(n != 0 ){
+            resultS += str;
+            n--;
+        }
+        return resultS;
+    }
+}
---
+++ src/warmUp02/StringTimes.java
@@ -0,0 +1,26 @@
+package warmUp02;
+
+public class StringTimes {
+    /**
+     Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+     stringTimes("Hi", 2) → "HiHi"
+     stringTimes("Hi", 3) → "HiHiHi"
+     stringTimes("Hi", 1) → "Hi"
+     */
+
+    public static void main(String[] args) {
+        System.out.println(stringTimes("Hi", 2));
+    }
+
+    public static String stringTimes(String str, int n) {
+        String part = "";
+
+        while(n != 0 ){
+            part += str;
+            n--;
+        }
+
+        return part;
+    }
+}
